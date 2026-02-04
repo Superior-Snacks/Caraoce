73ce09a442c04b66353c8f481dd2c2585e5be0c3
Superior-Snacks <joningiwork@gmail.com>
2026-02-04 16:50:36 +0000

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Plugin.Maui.Audio;
+using System.IO;
+
+namespace Caraoce
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void OnPlayClicked(object sender, EventArgs e)
+        {
+            var audioStream = await FileSystem.OpenAppPackageFileAsync("mysong.mp3");
+
+            // CHANGED: No 'await', and removed 'Async' from the name
+            var player = AudioManager.Current.CreatePlayer(audioStream);
+            PlayButton.Text = "Playing...";
+            player.Play();
+        }
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using Plugin.Maui.Audio;
+
+namespace Caraoce
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            // --- THE FIX ---
+            // Instead of .AddAudio(), we manually tell it to use the Current Audio Manager
+            builder.Services.AddSingleton(AudioManager.Current);
+
+            return builder.Build();
+        }
+    }
+}
