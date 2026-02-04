3c6645c4f3d9a4fa73852058d3bcdc82db625bd6
Superior-Snacks <joningiwork@gmail.com>
2026-02-04 18:03:32 +0000

    s

--- StartPage.xaml.cs
+++ StartPage.xaml.cs
@@ -2,8 +2,21 @@ namespace Caraoce;
 
 public partial class StartPage : ContentPage
 {
-	public StartPage()
-	{
-		InitializeComponent();
-	}
+    public StartPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnShuffleClicked(object sender, EventArgs e)
+    {
+        // We will add logic here later to pick a random song
+        // For now, let's just push the Lyrics page to see it work
+        Navigation.PushAsync(new MainPage());
+    }
+
+    private void OnSelectClicked(object sender, EventArgs e)
+    {
+        // This will eventually go to a Song List page
+        DisplayAlertAsync("Selection", "Song list coming soon!", "OK");
+    }
 }
